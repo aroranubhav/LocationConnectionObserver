3fe1c3d6df76800e4253482e95276654bd3c4c24
maxi <arora.anubhav93@gmail.com>
2025-06-25 23:07:45 +0530

    Add entry class to the library, which provides the state as flow

---
+++ locationobserver/src/main/java/com/maxi/locationobserver/LocationObserver.kt
@@ -0,0 +1,55 @@
+package com.maxi.locationobserver
+
+import android.content.Context
+import android.util.Log
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class LocationObserver(
+    private val context: Context
+) {
+
+    private lateinit var locationReceiver: LocationReceiver
+
+    private val _locationState = MutableStateFlow<LocationState>(LocationState.Unavailable)
+    val locationState = _locationState.asStateFlow()
+
+    init {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        _locationState.value = LocationStateProvider.getLocationState(context)
+        registerLocationReceiver()
+    }
+
+    private fun registerLocationReceiver() {
+        val locationCallback = object : LocationCallback {
+            override fun onLocationEnabled() {
+                _locationState.value = LocationState.Available
+            }
+
+            override fun onLocationDisabled() {
+                _locationState.value = LocationState.Unavailable
+            }
+
+            override fun onErrorReceived() {
+                Log.d(TAG, "onErrorReceived: ")
+                _locationState.value = LocationState.Unavailable
+            }
+        }
+
+        if (!::locationReceiver.isInitialized) {
+            locationReceiver = LocationReceiver(context, locationCallback)
+            locationReceiver.registerLocationReceiver()
+        }
+    }
+
+    fun unregisterLocationReceiver() {
+        if (::locationReceiver.isInitialized) {
+            locationReceiver.unregisterLocationReceiver()
+        }
+    }
+}
+
+const val TAG = "LocationObserverTAG"
